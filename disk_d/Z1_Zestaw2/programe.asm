aac750cc7a3edfe91d62e81eec675692bb37eef3
Hub1X <hubert.poteraj@gmail.com>
2026-03-25 21:28:09 +0100

    Add exe program and separate compiler

--- disk_d/Z1_Zestaw2/programe.asm
+++ disk_d/Z1_Zestaw2/programe.asm
@@ -2,10 +2,10 @@
 ;                                                                             ;
 ; Plik           : arch1-2e.asm                                               ;
 ; Format         : EXE                                                        ;
-; Cwiczenie      : Kompilacja, konsolidacja i debugowanie program�w           ;
+; Cwiczenie      : Kompilacja, konsolidacja i debugowanie programow           ;
 ;                  asemblerowych                                              ;
-; Autorzy        : Imie Nazwisko, Imie Nazwisko, grupa, dzien, godzina zajec  ;
-; Data zaliczenia: DD.MM.ROK                                                  ;
+; Autorzy        : Hubert Poteraj, Michal Pawlowski, 3, Wtorek, 10:30-12:00   ;
+; Data zaliczenia: 26.03.2026                                                  ;
 ; Uwagi          : Program obliczajacy wzor: (3*a-b/a)*(d+3)                  ;
 ;                                                                             ;
 ;=============================================================================;
@@ -18,40 +18,47 @@ a               DB      20
 b               DB      10
 c               DB      3
 d               DB      5
-Wynik           DB      ?
+Wynik           DW      ?
 
 Dane            ENDS
 
 Kod             SEGMENT
 
-                ASSUME   CS:Kod, DS:Dane, SS:Stos
+                ASSUME   CS:Kod, DS:Dane, SS:Stosik
 
 Start:
-                mov     ax, ds
                 mov     ax, SEG Dane
-
-                mov     ah, a
+				mov     ds, ax
+				
+				;bx = 3*a
+				mov     al, a
                 mul     c
-                shl     ax, 2
-                add     ah, a
-                mov     ax, ax
-                div     c
-                mov     ax, b
-                sub     dx, ax
-                mul     dl
-                mov     al, d
-                add     al, 07h
+				mov     bx, ax
+				
+				;ax = b/a
+                mov     al, b
+				div		a
+				mov		ah, 00h
+				
+				;bx = 3*a-b/a
+                sub     bx, ax
+				
+				;ax = d+3
+				mov		al, d
+				add		al, 3
+				
+				;ax = (3*a-b/a)*(d+3)
+                mul     bx
+				
+                mov     Wynik, ax
 
-                mov     ax, WORD PTR Wynik
-
-                mov     ax, 4C5h
+                mov     ax, 4C00h
                 int     21h
 
-Stos            SEGMENT
-                ;STACK 100h
+Stosik            SEGMENT
 
-                ;DB      100h DUP [?]
-Stos            ENDS
+				DB      100h DUP (?)
+Stosik          ENDS
 
 Kod             ENDS
 
